c8722fb3aeb48e0d85b6037befc42df55bdad94b
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-04-25 23:45:44 +0100

    removed xchart

--- app/services/ChartService.scala
+++ app/services/ChartService.scala
@@ -3,9 +3,6 @@ package services
 import models.FuelStation
 
 import javax.inject.*
-import org.knowm.xchart.*
-import org.knowm.xchart.style.markers.SeriesMarkers
-import org.knowm.xchart.VectorGraphicsEncoder.VectorGraphicsFormat
 import play.api.libs.json.{Json, OFormat}
 
 import java.awt.Color
@@ -25,84 +22,6 @@ object Series {
 }
 
 class ChartService @Inject()(getSqlQueries: GetSqlQueries)(implicit ec: ExecutionContext) {
-
-  def priceHistoryChart(fuelStation: FuelStation): Future[Array[Byte]] = {
-
-    val colors = List(Color.RED, Color.BLUE, Color.GREEN, Color.BLACK, Color.GRAY)
-
-    getSqlQueries.findPricesForStation(fuelStation.nodeId).map { prices =>
-
-      val chart = new XYChartBuilder()
-        .width(2000)
-        .height(800)
-        .title(s"Fuel Prices for ${fuelStation.tradingName} (${fuelStation.location.postcode})")
-        .xAxisTitle("Date")
-        .yAxisTitle("Price (pences)")
-        .build()
-
-      val styler = chart.getStyler
-
-      // ✅ IMPORTANT: use Date axis mode
-      styler.setPlotContentSize(0.9)
-      styler.setDatePattern("dd MMM yyyy")
-      styler.setXAxisLabelRotation(45)
-      styler.setChartFontColor(Color.BLACK)
-
-      styler.setPlotMargin(20)
-      styler.setChartTitleVisible(true)
-
-      styler.setChartTitleFont(
-        new java.awt.Font("Arial", java.awt.Font.BOLD, 38)
-      )
-
-      styler.setAxisTickLabelsFont(
-        new java.awt.Font("Arial", java.awt.Font.PLAIN, 38)
-      )
-
-      styler.setAxisTitleFont(
-        new java.awt.Font("Arial", java.awt.Font.BOLD, 40)
-      )
-
-      styler.setLegendFont(
-        new java.awt.Font("Arial", java.awt.Font.PLAIN, 38)
-      )
-
-      styler.setLegendPosition(org.knowm.xchart.style.Styler.LegendPosition.OutsideS)
-      styler.setLegendLayout(org.knowm.xchart.style.Styler.LegendLayout.Horizontal)
-
-      val grouped = prices.groupBy(_.fuelType).toSeq
-
-      grouped.zipWithIndex.foreach { case ((fuelType, pts), idx) =>
-
-        val xData = pts
-          .map(_.priceChangeEffectiveTimestamp.toEpochMilli.toDouble)
-          .sorted
-          .toArray
-
-        val yData = pts
-          .sortBy(_.priceChangeEffectiveTimestamp)
-          .map(_.price)
-          .toArray
-
-        val series = chart.addSeries(fuelType.displayText, xData, yData)
-
-        // line + dots
-        series.setMarker(SeriesMarkers.CIRCLE)
-        series.setLineColor(colors(idx % colors.size))
-        series.setLineWidth(2.0f)
-      }
-
-      val baos = new ByteArrayOutputStream()
-
-      VectorGraphicsEncoder.saveVectorGraphic(
-        chart,
-        baos,
-        VectorGraphicsFormat.SVG
-      )
-
-      baos.toByteArray
-    }
-  }
 
   def priceHistoryData(nodeId: String): Future[Seq[Series]] =
     getSqlQueries.findPricesForStation(nodeId).map { prices =>
