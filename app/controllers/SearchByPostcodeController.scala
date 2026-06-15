5b97dedb704289fc2af8db41a05dcd40fe74a83d
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-06-15 16:43:02 +0100

    Refactor debug logging for traceparent header

--- app/controllers/SearchByPostcodeController.scala
+++ app/controllers/SearchByPostcodeController.scala
@@ -142,7 +142,7 @@ class SearchByPostcodeController @Inject()(
 
   def showNearbyFuelStations(postcode: String, fuelType: FuelType, radius: Double): Action[AnyContent] = authAction.async { implicit request =>
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequestAndSession(request, request.session)
-    logger.debug("traceparent=" + request.headers.get("traceparent"))
+    logger.debug(s"traceparent= ${request.headers.get("traceparent")}")
     
     searchByPostcodeService.getViewModel(postcode, fuelType, radius.min(100.0)).fold(
       error => error match {
